bfcf23dfe6294edb9d9c5036114e3e58bea194b4
home-Ubuntu <fc.dd.omg@gmail.com>
2019-08-13 20:50:38 +0900

    2019/8/13

--- python/src/opencv/edges.py
+++ python/src/opencv/edges.py
@@ -13,12 +13,6 @@ class edges:
         dst = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
         edges = cv2.Canny(dst,75,25,3)
 
-        threshold = 60
-        minLineLength = 10
-        lines = cv2.HoughLinesP(edges,1,np.pi/180,threshold,minLineLength,10)
-        for x1,y1,x2,y2 in lines[0]:
-            cv2.line(edges,(x1,y1),(x2,y2),(0,255,0),2)
-
         cv2.imshow("結果画像",edges)
 
         if cv2.waitKey() == ord("s"):
@@ -34,6 +28,6 @@ args = sys.argv
 if len(args) > 1:
     edges = edges(args[1])
 else:
-    edges = edges("./image/angle1.png")
+    edges = edges("./image/test.jpg")
 
 edges.cannyEdges()
--- python/src/opencv/hough.py
+++ python/src/opencv/hough.py
@@ -11,13 +11,29 @@ class hough:
     def conversion(self):
         image = cv2.imread(self.name)
         cv2.imshow("元画像",image)
-        minLineLength = 100
-        maxLineGap = 10
-        lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
-        for x1,y1,x2,y2 in lines[0]:
+        gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(gray,30,40,apertureSize=3)
+        cv2.imshow("エッジ画像",edges)
+
+        #長い線の検出
+        lines = cv2.HoughLinesP(edges,1,np.pi/180,threshold=200,minLineLength=200,maxLineGap=50)
+        #短い線の検出
+        sLines = cv2.HoughLinesP(edges,1,np.pi/180,threshold=200,minLineLength=5,maxLineGap=5)
+
+        dst = cv2.cvtColor(edges,cv2.COLOR_GRAY2BGR)
+        for line in lines:
+            x1,y1,x2,y2 = line[0]
             cv2.line(dst,(x1,y1),(x2,y2),(0,0,255),2)
 
+        for line in sLines:
+            x1,y1,x2,y2 = line[0]
+            cv2.line(dst,(x1,y1),(x2,y2),(0,0,255),2)
+
+
         cv2.imshow("結果画像",dst)
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
     
 
 args = sys.argv
