2bbaf5d254b90b432d7ecc0cc73f74e37db518eb
home-Ubuntu <fc.dd.omg@gmail.com>
2019-09-15 00:41:20 +0900

    2019/9/14

--- Learning/src/LineDitection.py
+++ Learning/src/LineDitection.py
@@ -1,12 +1,13 @@
 import cv2
 import numpy as np
 import sys
+import glob
 
 class diteciton:
 
-    def __init__(self,image,name):
-        self.image = image #画像オブジェクト
-        self.name = name #画像ファイル名
+    #コンストラクタ
+    def __init__(self,name):
+        self.name = name
 
     #フィールド部分の抽出    
     def fieldDitection(self,image):
@@ -15,7 +16,7 @@ class diteciton:
 
         hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
         dst = cv2.inRange(hsv,lower,upper)
-        cv2.imshow("抽出フィールド",dst)
+        #cv2.imshow("抽出フィールド",dst)
 
         return dst
 
@@ -34,7 +35,7 @@ class diteciton:
         # dst = cv2.erode(dst,kernel,iterations = 30)
 
         dst = cv2.cvtColor(dst,cv2.COLOR_GRAY2BGR)
-        cv2.imshow("ノイズ除去",dst)
+        #cv2.imshow("ノイズ除去",dst)
 
         return dst
 
@@ -44,17 +45,27 @@ class diteciton:
         gmask = cv2.cvtColor(mask,cv2.COLOR_BGR2GRAY)
         dst = cv2.bitwise_and(image,image,mask=gmask) 
 
-        cv2.imshow("マスク結果",dst)
+        #cv2.imshow("マスク結果",dst)
 
         return dst
     
     #エッジ抽出後にハフ変換
     def hough(self,image):
+        #２値化
         gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        #エッジ抽出
         edges = cv2.Canny(gray,30,40,apertureSize=3)
-        cv2.imshow("エッジ画像",edges)
+        #平滑化
+        #edges = cv2.GaussianBlur(edges,(1,1),10)
 
-        lines = cv2.HoughLinesP(edges,1,np.pi/180,threshold=90,minLineLength=10,maxLineGap=50)
+        #ノイズ除去
+        # kernel = np.ones((3,3),np.uint8)
+        # edges = cv2.dilate(edges,kernel,iterations = 3)
+        #edges = cv2.erode(edges,kernel,iterations = 3)
+
+        #cv2.imshow("エッジ画像",edges)
+
+        lines = cv2.HoughLinesP(edges,1,np.pi/180,threshold=90,minLineLength=50,maxLineGap=30)
 
         dst = cv2.cvtColor(edges,cv2.COLOR_GRAY2BGR)
 
@@ -62,7 +73,7 @@ class diteciton:
             x1,y1,x2,y2 = line[0]
             cv2.line(dst,(x1,y1),(x2,y2),(0,0,255),2)
 
-        cv2.imshow("ライン抽出",dst)
+        #cv2.imshow("ライン抽出",dst)
 
         lower = np.array([0,128,0])
         upper = np.array([30,255,255])
@@ -75,14 +86,21 @@ class diteciton:
 
     #メイン関数
     def main(self):
-        mask = self.fieldDitection(self.image)
-        mask = self.morphology(mask)
-        maskedImage = self.masking(self.image,mask)
-        line = self.hough(maskedImage)
+        count = 1
+        for x in glob.glob("./image/datasets/" + name + "/*.jpg"):
 
-        cv2.imwrite("./result/resut.jpg",line)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+            image = cv2.imread(x)
+            
+            mask = self.fieldDitection(image)
+            mask = self.morphology(mask)
+            maskedImage = self.masking(image,mask)
+            line = self.hough(maskedImage)
+
+            cv2.imwrite("./result/" + name + "/line_" + str(count) + ".jpg",line)
+            count += 1
+
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
         
 
 if __name__=="__main__":
@@ -90,7 +108,7 @@ if __name__=="__main__":
     if len(args) > 1:
         name = args[1]
     else:
-        name = "./image/datasets/right/result_00295.jpg"
-    image = cv2.imread(name)
-    dt = diteciton(image,name)
+        name = "test"
+
+    dt = diteciton(name)
     dt.main()
